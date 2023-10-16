e6f94bcc9375cc2dd13955ca38b434a0470b9e39
bruno bento <66705552+BDBento@users.noreply.github.com>
2023-10-16 17:02:17 -0400

    get seter

--- entity-crud/src/main/java/com/estudante/entitycrud/modelo/Estudante.java
+++ entity-crud/src/main/java/com/estudante/entitycrud/modelo/Estudante.java
@@ -22,4 +22,30 @@ public class Estudante {
 	
 	@Min(value = 18,message =  "o aluno deve ter no minimo 18 anos" )
 	private int idade;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	
 }
