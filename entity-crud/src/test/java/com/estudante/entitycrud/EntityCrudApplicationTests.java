722636aad158eed963396530fb85ae2df3bdd75d
bruno bento <66705552+BDBento@users.noreply.github.com>
2023-10-16 16:40:11 -0400

    comecando

---
+++ entity-crud/src/test/java/com/estudante/entitycrud/EntityCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudante.entitycrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
