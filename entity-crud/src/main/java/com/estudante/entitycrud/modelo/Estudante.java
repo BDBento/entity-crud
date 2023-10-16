5fff2fa6fdbc43a0d0cdd050f92ebbbb4a37ab32
bruno bento <66705552+BDBento@users.noreply.github.com>
2023-10-16 17:00:34 -0400

    foi

--- entity-crud/src/main/java/com/estudante/entitycrud/modelo/Estudante.java
+++ entity-crud/src/main/java/com/estudante/entitycrud/modelo/Estudante.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 
@@ -13,4 +16,10 @@ public class Estudante {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 
+	@NotBlank(message = "o nome nao foi digitado")
+	@Size(min = 2, message = "nome muito curto")
+	private String nome;
+	
+	@Min(value = 18,message =  "o aluno deve ter no minimo 18 anos" )
+	private int idade;
 }
